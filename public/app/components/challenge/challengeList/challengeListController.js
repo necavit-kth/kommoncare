77096637bd060a72c72b131075ca4ee3ef1c41be
E-vanderHeide <Ewoud.vanderheide@outlook.com>
2016-02-13 11:51:34 +0100

    Challenge list

--- public/app/components/challenge/challengeList/challengeListController.js
+++ public/app/components/challenge/challengeList/challengeListController.js
@@ -1,5 +1,9 @@
+ 
  angular.module('app.challengeListModule', [])
- .controller('challengeListController', function($scope) {
-        // create a message to display in our view
-        $scope.message = 'ChallengeList!';
+ .controller('challengeListController', function($scope, $http) {
+
+		$http.get("http://kommoncare.davidmr.es/api/challenges/")
+	    .then(function(response) {
+	    	$scope.challenges = response.data;
+	    });
     });
