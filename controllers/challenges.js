fc8fb8731032e62aebcabb49d9a7ccd3b540cf5e
necavit <gollumdeagol@gmail.com>
2016-02-13 11:28:25 +0100

    lock and complete challenges

--- controllers/challenges.js
+++ controllers/challenges.js
@@ -1,6 +1,7 @@
 var Challenge = require('../models/challenge');
 var Category = require('../models/category');
 var User = require('../models/user');
+var ObjectId = require('mongoose').Types.ObjectId;
 
 module.exports = {
 
@@ -53,41 +54,92 @@ module.exports = {
       if (err) throw err;
       res.sendStatus(204);
     });
+  },
+
+  //POST /challenges/:id/action/:action[{lock|complete}]
+  applyActionToChallenge: function(req, res, next) {
+    switch (req.params.action) {
+      case "lock":
+        //get the challenge
+        Challenge.findById(req.params.id,function (err, challenge) {
+          if (err) throw err;
+          // update the challenge copy state and save
+          challenge.state = "locked";
+          challenge.lockerId = req.body.lockerId;
+          challenge.save(function(err){
+            if (err) throw err;
+            // update the owner's copy
+            User.findById(challenge.ownerId, function(err, owner) {
+              if (err) throw err;
+              var published = owner.challenges.published;
+              for (var i = 0; i < published.length; i++) {
+                if (published[i]._id == req.params.id) {
+                  published.splice(i,1,challenge);
+                  break;
+                }
+              }
+              owner.save(function(err){
+                if (err) throw err;
+                // create the locker's copy in "active" challenges
+                User.findById(req.body.lockerId, function(err, locker) {
+                  if (err) throw err;
+                  locker.challenges.active.push(challenge);
+                  locker.save(function(err){
+                    if (err) throw err;
+                    res.sendStatus(200);
+                  });
+                });
+              });
+            });
+          });
+        });
+        break;
+      case "complete":
+        //get the challenge
+        Challenge.findById(req.params.id,function (err, challenge) {
+          if (err) throw err;
+          // update the challenge copy state and save
+          challenge.state = "completed";
+          challenge.save(function(err){
+            if (err) throw err;
+            // update the owner's copy
+            User.findById(challenge.ownerId, function(err, owner) {
+              if (err) throw err;
+              var published = owner.challenges.published;
+              for (var i = 0, len = published.length; i < len; i++) {
+                if (published[i]._id == req.params.id) {
+                  published.splice(i,1,challenge);
+                  break;
+                }
+              }
+              owner.save(function(err){
+                if (err) throw err;
+                // create the locker's copy in "active" challenges
+                User.findById(challenge.lockerId, function(err, locker) {
+                  if (err) throw err;
+                  var activeChallenges = locker.challenges.active;
+                  for (var i = 0, len = activeChallenges.length; i < len; i++) {
+                    if (activeChallenges[i]._id == req.params.id) {
+                      activeChallenges.splice(i,1);
+                      break;
+                    }
+                  }
+                  locker.challenges.completed.push(challenge);
+                  locker.save(function(err){
+                    if (err) throw err;
+                    //TODO add awards thing!
+                    res.sendStatus(200);
+                  });
+                });
+              });
+            });
+          });
+        });
+        break;
+      default:
+        res.sendStatus(400);
+        break;
+    }
   }
 
-  //POST /challenges/:id/action/{lock|complete}
-  // applyActionToChallenge: function(req, res, next) {
-  //
-  // }
-
 };
-
-/*
-//POST /users
-createUser: function(req, res, next) {
-  // create a new user
-  var newUser = User(req.body);
-
-  // save the user
-  newUser.save(function(err) {
-    if (err) throw err;
-    res.send('User: ' + req.body.name + ' has been created!');
-  });
-},
-
-// GET /users
-getUsers: function(req, res, next) {
-  User.find({}, function(err, users) {
-    if (err) throw err;
-    res.status(200).json(users);
-  });
-},
-
-// GET /users/:id
-getUser: function(req, res, next) {
-  User.findById(req.params.id, function(err, user) {
-    if (err) throw err;
-    res.status(200).json(user);
-  });
-},
-*/
